208404f7a6fe08c6b7450f767586e00b051803f6
MartyRizey <martyrizey@gmail.com>
2020-05-06 19:50:32 +0200

    Ajout fichier home.php et modif index.php

--- LaPlaceDuMarche_phil/public/index.php
+++ LaPlaceDuMarche_phil/public/index.php
@@ -1,21 +1,6 @@
-<?php 
-	require '../templates/doctype.php';
-	require '../templates/header.php'; 
-	
-	require '../templates/navbar.php'; 
-  require '../templates/carousel_home.php';    
-  require '../templates/avantageService.php';
-	
-	require '../templates/mainHome_shopNow.php';
-	require '../templates/mainHome_oursProducts.php';
-	require '../templates/mainHome_dealOfTheDay.php';	
+<?php
 
-	require '../templates/satisfiedCustomer.php'; 
-  require '../templates/partner_link_home.php';  
-  require '../templates/newsLetter_form.php';
+	require '../views/home.php';
 
-	require '../templates/footer_home.php';
-	require '../templates/footer_script.php';
-	require '../templates/footerTagClosed.php'; 	
-?>
+	
     
