c3f9fca22cd347d0f12e4fcadfa20b63dd2712c5
MartyRizey <martyrizey@gmail.com>
2020-05-06 07:53:36 +0200

    Ajout dossier structure code - Philippe

---
+++ LaPlaceDuMarche_phil/public/index.php
@@ -0,0 +1,21 @@
+<?php 
+	require '../templates/doctype.php';
+	require '../templates/header.php'; 
+	
+	require '../templates/navbar.php'; 
+  require '../templates/carousel_home.php';    
+  require '../templates/avantageService.php';
+	
+	require '../templates/mainHome_shopNow.php';
+	require '../templates/mainHome_oursProducts.php';
+	require '../templates/mainHome_dealOfTheDay.php';	
+
+	require '../templates/satisfiedCustomer.php'; 
+  require '../templates/partner_link_home.php';  
+  require '../templates/newsLetter_form.php';
+
+	require '../templates/footer_home.php';
+	require '../templates/footer_script.php';
+	require '../templates/footerTagClosed.php'; 	
+?>
+    
